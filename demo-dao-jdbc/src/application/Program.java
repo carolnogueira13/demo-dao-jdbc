5f0b25bc159f408f55ce950c78b572557a06176d
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-02-02 15:51:53 -0300

    insert implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -37,6 +38,16 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+
+
+		Seller newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, dp);
+		
+		sellerDao.insert(newseller);
+		
+		System.out.println("Inserted! New ID = " + newseller.getId());
+		
 
 	}
 
