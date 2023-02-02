d12860b2e3b01d51a5a2e8a7d21a402f93b5677e
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-02-02 15:27:47 -0300

    findAll implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -10,27 +10,33 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findById ===");
-		
+
 		Seller seller = sellerDao.findById(3);
-		
+
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TEST 2: seller findByDepartment ===");
-		
+
 		Department dp = new Department(2, null);
-		
+
 		List<Seller> list = sellerDao.findByDepartment(dp);
-		
-		for (Seller obj: list) {
+
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		
-		
+
+		System.out.println("\n=== TEST 3: seller findAll ===");
+
+
+		list = sellerDao.findAll();
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
