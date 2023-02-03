42173d023667d250c36b66792f87cec1355325c3
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-02-03 12:42:55 -0300

    update implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -48,7 +48,13 @@ public class Program {
 		
 		System.out.println("Inserted! New ID = " + newseller.getId());
 		
+		System.out.println("\n=== TEST 5: seller update ===");
 
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
 	}
 
 }
